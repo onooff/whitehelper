e531aa9442b51be132c7ad6f96c4fe2f7d50b1e4
Kim Young Min <kastori1990@gmail.com>
2023-01-16 00:05:21 +0900

    update 하우스 content 레이아웃 수정

--- src/components/card/HouseContent.jsx
+++ src/components/card/HouseContent.jsx
@@ -2,25 +2,28 @@ import React from "react";
 import Typography from "@mui/material/Typography";
 import { Box } from "@mui/material";
 import StarRateIcon from "@mui/icons-material/StarRate";
-import Stack from "@mui/material/Stack";
 import Link from "@mui/material/Link";
+import SavingsTwoToneIcon from "@mui/icons-material/SavingsTwoTone";
+import { Stack } from "@mui/system";
 
 export default function HouseContent({ id, locate, rate, price }) {
   return (
     <Box>
       <Link href={`detail/${id}`} underline="none" color="black">
-        <Typography variant="h7" component="div" align="left">
+        <Typography variant="h7" component="div" align="left" mb={0.5}>
           {locate}
         </Typography>
 
-        <Typography variant="body2" component="div" align="right">
-          <StarRateIcon sx={{ fontSize: "small" }} />
-          {rate}
-        </Typography>
-
-        <Typography gutterBottom variant="h7" component="div">
-          {price}
-        </Typography>
+        <Stack direction="row" justifyContent="space-between">
+          <Typography gutterBottom variant="h7" component="div">
+            <SavingsTwoToneIcon sx={{ fontSize: "medium" }} />
+            {price}
+          </Typography>
+          <Typography variant="body2" component="div" align="right">
+            <StarRateIcon sx={{ fontSize: "small" }} />
+            {rate}
+          </Typography>
+        </Stack>
       </Link>
     </Box>
   );
