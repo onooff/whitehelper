6271ed5d623a9140c673bb3af91acfd26c7ac9df
JeongHun Han <onooff@naver.com>
2023-01-15 18:36:21 +0900

    Revert "Dev"

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import Layout from "./Layouts";
-import Main from "./pages/Main";
 
-export default function App() {
-  return <Layout chlidren={<Main />}></Layout>;
+
+function App() {
+  return (
+    <div>
+      hello world
+    </div>
+  );
 }
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router";
-import { router } from "./routes";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-
-  <RouterProvider router={router} />
-
+    <App />
   // </React.StrictMode>
 );
