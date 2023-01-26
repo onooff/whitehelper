5c12096b313836b818c37e1e8bf4a91fad6a1c6c
Kim Young Min <kastori1990@gmail.com>
2023-01-26 14:20:48 +0900

    update 달력 수정 이전날짜 클릭 금지

--- src/components/detail/CustomDatePicker.jsx
+++ src/components/detail/CustomDatePicker.jsx
@@ -5,6 +5,8 @@ import { DateRange } from "react-date-range";
 import "moment/locale/ko";
 import ko from "date-fns/locale/ko";
 import "./detail.css";
+import { addDays } from "date-fns";
+
 export default function CustomDatePicker({ state, setState }) {
   return (
     <>
@@ -13,11 +15,13 @@ export default function CustomDatePicker({ state, setState }) {
         showSelectionPreview={true}
         moveRangeOnFirstSelection={false}
         months={2}
+        minDate={addDays(new Date(), 0)}
         ranges={state}
         direction="horizontal"
         locale={ko}
         rangeColors={["black", "black", "black", "black"]}
         color="black"
+        dateDisplayFormat="yyyy년 MM월 dd일"
       />
     </>
   );
