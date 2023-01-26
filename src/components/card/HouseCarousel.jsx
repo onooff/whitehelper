0ae4146cf2a912fc5d6f803b27f8d3ae1ac19f78
Kim Young Min <kastori1990@gmail.com>
2023-01-27 02:39:32 +0900

    add 예약 완료 페이지 및 예약 목록 페이지

--- src/components/card/HouseCarousel.jsx
+++ src/components/card/HouseCarousel.jsx
@@ -1,63 +1,54 @@
-import React, { useState } from 'react';
-import Carousel from 'react-material-ui-carousel';
-import { styled } from '@mui/material/styles';
-import FavoriteTwoToneIcon from '@mui/icons-material/FavoriteTwoTone';
-import IconButton from '@mui/material/IconButton';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import Box from '@mui/material/Box';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import Carousel from "react-material-ui-carousel";
+import { styled } from "@mui/material/styles";
+import FavoriteTwoToneIcon from "@mui/icons-material/FavoriteTwoTone";
+import IconButton from "@mui/material/IconButton";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import Box from "@mui/material/Box";
+import { Link } from "react-router-dom";
 
-const StyledProductImg = styled('img')({
+const StyledProductImg = styled("img")({
   top: 0,
-  width: '100%',
-  height: '100%',
-  objectFit: 'cover',
-  borderRadius: '10px',
-  position: 'relative',
+  width: "100%",
+  height: "100%",
+  objectFit: "cover",
+  borderRadius: "10px",
+  position: "relative",
 });
 
-export default function HouseCarousel({ img, id, member, setMember }) {
+export default function HouseCarousel({ img, id }) {
   const [isHovering, setIsHovering] = useState(true);
-  const [renderTrigger, setRenderTrigger] = useState(true);
+  const [isLike, setLike] = useState(false);
   const iconClickHandler = () => {
-    setMember((m) => {
-      if (m.favorite.has(id)) {
-        m.favorite.delete(id);
-      } else {
-        m.favorite.add(id);
-      }
-      console.log('action');
-      return m;
-    });
-    setRenderTrigger((prev) => !prev);
+    setLike((prev) => !prev);
   };
 
   return (
     <Box
       sx={{
-        width: '100%',
-        position: 'relative',
+        width: "100%",
+        position: "relative",
       }}
       onMouseOver={() => setIsHovering(false)}
       onMouseOut={() => setIsHovering(true)}
     >
       <Carousel
-        height={'273px'}
+        height={"273px"}
         autoPlay={false}
         indicatorContainerProps={{
           style: {
             zIndex: 1,
-            marginTop: '-30px',
-            position: 'relative',
-            padding: '10px',
+            marginTop: "-30px",
+            position: "relative",
+            padding: "10px",
           },
         }}
         animation="slide"
         cycleNavigation={false}
         navButtonsProps={{
           style: {
-            color: 'black',
-            backgroundColor: 'white',
+            color: "black",
+            backgroundColor: "white",
           },
         }}
         navButtonsAlwaysVisible={!isHovering}
@@ -67,24 +58,16 @@ export default function HouseCarousel({ img, id, member, setMember }) {
         ))}
       </Carousel>
       <IconButton
-        onClick={member ? iconClickHandler : null}
+        onClick={iconClickHandler}
         sx={{
           zIndex: 9,
           top: 10,
           right: 10,
-          position: 'absolute',
-          color: 'white',
+          position: "absolute",
+          color: "white",
         }}
       >
-        {member ? (
-          member.favorite.has(id) ? (
-            <FavoriteIcon sx={{ color: 'hotpink' }} />
-          ) : (
-            <FavoriteTwoToneIcon />
-          )
-        ) : (
-          <FavoriteTwoToneIcon />
-        )}
+        {isLike ? <FavoriteIcon color="secondary" /> : <FavoriteTwoToneIcon />}
       </IconButton>
     </Box>
   );
